a8b086aaeeb776d2f0024c48c984ccfcaed86c1e
Jill Donahue <jillmd501@gmail.com>
2015-10-06 13:09:23 -0600

    I3: number guessing game complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+SECRET_NUMBER = rand(100)
+
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess"]
+  message = check_guess(guess)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
 end
